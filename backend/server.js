ecede2d05f69ece4327bf782ae9f39d341b8d6cc
ShrihariJ2124 <shriharikodiyala04@gmail.com>
2026-03-24 13:50:11 +0530

    Update backend root response details

--- backend/server.js
+++ backend/server.js
@@ -43,8 +43,8 @@ app.get("/", (req, res) => {
   res.json({
     project: "MERN Chat App using Socket.IO",
     message: "Welcome to MERN Chat Application",
-    developedBy: "MasynTech",
-    website: "www.masynctech.com",
+    developedBy: "Shrihari J",
+    website: "https://shriharij2124.github.io/my-portfolio/",
   });
 });
 app.use("/api/users", userRouter);
